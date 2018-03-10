c44b5daceff9e1aaccbc76ee568c90269f5c7b03
devdack <devdack@gmail.com>
2018-03-10 13:11:40 +0700

    inflation model

--- programs/util/inflation_model.cpp
+++ programs/util/inflation_model.cpp
@@ -2,24 +2,17 @@
 #include <steemit/chain/compound.hpp>
 #include <steemit/protocol/asset.hpp>
 #include <steemit/protocol/types.hpp>
-
 #include <fc/io/json.hpp>
 #include <fc/variant_object.hpp>
 
 #include <iostream>
 #include <vector>
 
-#define CURATE_OFF      0
-#define VCURATE_OFF     1
-#define CONTENT_OFF     2
-#define VCONTENT_OFF    3
-#define PRODUCER_OFF    4
-#define VPRODUCER_OFF   5
-#define LIQUIDITY_OFF   6
-#define VLIQUIDITY_OFF  7
-#define POW_OFF         8
-#define VPOW_OFF        9
-#define REWARD_TYPES   10
+#define CONTENT      0
+#define PRODUCER     1
+#define VESTING      2
+#define LIQUIDITY    3
+#define REWARD_TYPES 4
 
 using steemit::protocol::asset;
 using steemit::protocol::share_type;
@@ -42,8 +35,6 @@ rvec shows total number of STEEM satoshis created since genesis for:
 - Vesting rewards balancing producer rewards
 - Liquidity rewards
 - Vesting rewards balancing liquidity rewards
-- PoW rewards
-- Vesting rewards balancing PoW rewards
 
 b is block number
 s is total supply
@@ -65,15 +56,8 @@ int main( int argc, char** argv, char** envp )
    std::vector< share_type > reward_delta;
    std::vector< share_type > reward_total;
 
-/*
-#define STEEMIT_GENESIS_TIME                    (fc::time_point_sec(1458835200))
-#define STEEMIT_MINING_TIME                     (fc::time_point_sec(1458838800))
-#define STEEMIT_FIRST_CASHOUT_TIME              (fc::time_point_sec(1467590400))  /// July 4th
-*/
-
-   uint32_t liquidity_begin_block = (1467590400 - 1458835200) / 3;
-   uint32_t pow_deficit = 100;
-
+   uint32_t liquidity_begin_block = STEEMIT_START_VESTING_BLOCK;
+   
    for( int i=0; i<REWARD_TYPES; i++ )
    {
       reward_delta.emplace_back();
@@ -82,51 +66,37 @@ int main( int argc, char** argv, char** envp )
 
    auto block_inflation_model = [&]( uint32_t block_num, share_type& current_supply )
    {
-      uint32_t vesting_factor = (block_num < STEEMIT_START_VESTING_BLOCK) ? 0 : 9;
+      int64_t start_inflation_rate = int64_t( STEEMIT_INFLATION_RATE_START_PERCENT );
+      int64_t inflation_rate_adjustment = int64_t( block_num / STEEMIT_INFLATION_NARROWING_PERIOD );
+      int64_t inflation_rate_floor = int64_t( STEEMIT_INFLATION_RATE_STOP_PERCENT );
 
-      share_type curate_reward   = calc_percent_reward_per_block< STEEMIT_CURATE_APR_PERCENT >( current_supply );
-      reward_delta[ CURATE_OFF ] = std::max( curate_reward, STEEMIT_MIN_CURATE_REWARD.amount );
-      reward_delta[ VCURATE_OFF ] = reward_delta[ CURATE_OFF ] * vesting_factor;
+      // below subtraction cannot underflow int64_t because inflation_rate_adjustment is <2^32
+      int64_t current_inflation_rate = std::max( start_inflation_rate - inflation_rate_adjustment, inflation_rate_floor );
 
-      share_type content_reward  = calc_percent_reward_per_block< STEEMIT_CONTENT_APR_PERCENT >( current_supply );
-      reward_delta[ CONTENT_OFF ] = std::max( content_reward, STEEMIT_MIN_CONTENT_REWARD.amount );
-      reward_delta[ VCONTENT_OFF ] = reward_delta[ CONTENT_OFF ] * vesting_factor;
+      auto new_steem = ( current_supply * current_inflation_rate ) / ( int64_t( STEEMIT_100_PERCENT ) * int64_t( STEEMIT_BLOCKS_PER_YEAR ) );
+      
+      reward_delta[CONTENT]  = ( new_steem * STEEMIT_CONTENT_REWARD_PERCENT ) / STEEMIT_100_PERCENT;
 
-      share_type producer_reward = calc_percent_reward_per_block< STEEMIT_PRODUCER_APR_PERCENT >( current_supply );
-      reward_delta[ PRODUCER_OFF ] = std::max( producer_reward, STEEMIT_MIN_PRODUCER_REWARD.amount );
-      reward_delta[ VPRODUCER_OFF ] = reward_delta[ PRODUCER_OFF ] * vesting_factor;
+      reward_delta[VESTING]  = ( new_steem * STEEMIT_VESTING_FUND_PERCENT ) / STEEMIT_100_PERCENT; /// 15% to vesting fund
+      
+      reward_delta[PRODUCER] =  new_steem - content_reward - vesting_reward; /// Remaining 10% to witness pay
 
-      current_supply += reward_delta[CURATE_OFF] + reward_delta[VCURATE_OFF] + reward_delta[CONTENT_OFF] + reward_delta[VCONTENT_OFF] + reward_delta[PRODUCER_OFF] + reward_delta[VPRODUCER_OFF];
-      // supply for above is computed by using pre-updated supply for computing all 3 amounts.
-      // supply for below reward types is basically a self-contained event which updates the supply immediately before the next reward type's computation.
+
+      current_supply += reward_delta[CONTENT] + reward_delta[VESTING] + reward_delta[PRODUCER];
+      
 
       share_type liquidity_reward = 0;
-      share_type pow_reward = 0;
+      
 
-      if( (block_num % STEEMIT_MAX_WITNESSES) == 0 )
-         ++pow_deficit;
-
-      if( pow_deficit > 0 )
-      {
-         pow_reward = calc_percent_reward_per_round< STEEMIT_POW_APR_PERCENT >( current_supply );
-         pow_reward = std::max( pow_reward, STEEMIT_MIN_POW_REWARD.amount );
-         if( block_num < STEEMIT_START_MINER_VOTING_BLOCK )
-            pow_reward *= STEEMIT_MAX_WITNESSES;
-         --pow_deficit;
-      }
-      reward_delta[ POW_OFF ] = pow_reward;
-      reward_delta[ VPOW_OFF ] = reward_delta[ POW_OFF ] * vesting_factor;
-
-      current_supply += reward_delta[ POW_OFF ] + reward_delta[ VPOW_OFF ];
-
-      if( (block_num > liquidity_begin_block) && ((block_num % STEEMIT_LIQUIDITY_REWARD_BLOCKS) == 0) )
+      if( (block_num > liquidity_begin_block) && (block_num < STEEMIT_LIQUIDITY_REWARD_BLOCKS))
       {
          liquidity_reward = calc_percent_reward_per_hour< STEEMIT_LIQUIDITY_APR_PERCENT >( current_supply );
          liquidity_reward = std::max( liquidity_reward, STEEMIT_MIN_LIQUIDITY_REWARD.amount );
       }
-      reward_delta[ LIQUIDITY_OFF ] = liquidity_reward;
-      reward_delta[ VLIQUIDITY_OFF ] = reward_delta[ LIQUIDITY_OFF ] * vesting_factor;
-      current_supply += reward_delta[ LIQUIDITY_OFF ] + reward_delta[ VLIQUIDITY_OFF ];
+      reward_delta[ LIQUIDITY] = liquidity_reward;
+      current_supply += reward_delta[ LIQUIDITY];
+
+
 
       for( int i=0; i<REWARD_TYPES; i++ )
       {
@@ -134,11 +104,15 @@ int main( int argc, char** argv, char** envp )
       }
 
       return;
+
    };
 
-   share_type current_supply = 0;
 
-   for( uint32_t b=1; b<10*STEEMIT_BLOCKS_PER_YEAR; b++ )
+
+   share_type current_supply = STEEMIT_INIT_SUPPLY;
+
+
+   for( uint32_t b=1; b<20*STEEMIT_BLOCKS_PER_YEAR; b++ )
    {
       block_inflation_model( b, current_supply );
       if( b%1000 == 0 )
